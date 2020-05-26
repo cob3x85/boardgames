8a9a80e180ca5f0efadd27d4143c3d03c4eba2e1
cob3x85 <cob3x85@outlook.com>
2020-05-26 11:00:33 -0500

    Set Firebase variables

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,14 +4,14 @@
 
 export const environment = {
   firebase: {
-    apiKey: 'AIzaSyBvtGOoedWavzYRJ7ElcxYPzRBp08L_s7s',
-    authDomain: 'redux-ingreso-egreso.firebaseapp.com',
-    databaseURL: 'https://redux-ingreso-egreso.firebaseio.com',
-    projectId: 'redux-ingreso-egreso',
-    storageBucket: 'redux-ingreso-egreso.appspot.com',
-    messagingSenderId: '151523350833',
-    appId: '1:151523350833:web:871bd34c7e8c40584f3810',
-    measurementId: 'G-5F6NN8LN6C'
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
+    measurementId: ''
   },
   images: 'memorama/',
   production: false
